aad6a893040f7a8a832839147565fd1fcd8803ee
Ben Miller <bmiller@rackspace.com>
2016-03-07 13:45:19 -0500

    spec helper for large texts in files
    in some cases it is inconvenient to put all router outputs in the spec
    file, so add a helper for loading these strings from external (plain
    text) files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
 def klass
   described_class
 end
+
+def text_fixture(name)
+  File.read(File.expand_path(__dir__ + "/../fixtures/#{name}.txt"))
+end
